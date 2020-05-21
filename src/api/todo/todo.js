0ddaff7498fb6b0dc0866abf4afff82c23f92ad7
Gabriel Testoni <gabriel.testoni@gmail.com>
2020-05-21 00:49:53 -0300

    mapping routes

--- src/api/todo/todo.js
+++ src/api/todo/todo.js
@@ -2,7 +2,7 @@ const restful = require('node-restful') // create under mongoose a rest
 const mongoose = restful.mongoose
 
 const todoSchema = new mongoose.Schema({
-    description: { type: String, required: treu },
+    description: { type: String, required: true },
     done: { type: Boolean, require: true, default: false },
     createAt: { type: Date, default: Date.now }
 })
